a9ca47e805e40f9464a9c83c0e2011650a8dee37
mariamtamerr <memariam5@outlook.com>
2023-11-05 15:20:20 +0200

    add deploy workflow

--- Alhekma/src/components/Home.jsx
+++ Alhekma/src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     <div className='parent w-full h-full '>
         <img className='lg:w-full w-[400px] lg:h-full h-[400px] absolute object-cover' src={Coming} alt='Coming Soon'/>
         <div className='w-full h-full  bg-black/80 absolute top-0 '></div>
-        <h1 className='z-10 md:text-[5rem] text-3xl text-gray-300 absolute top-[35%] left-[30%]'>Coming Soon</h1>
+        <h1 className='z-10 md:text-[5rem] text-3xl text-gray-300 absolute top-[34.9%] left-[30%]'>Coming Soon</h1>
     </div>
     
 
