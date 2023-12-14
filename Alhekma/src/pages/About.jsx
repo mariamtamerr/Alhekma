7a028589e3f3f0da4baf83b4d119e129e9cb452a
mariamtamerr <memariam5@outlook.com>
2023-12-14 18:14:55 +0200

    Last Changes

--- Alhekma/src/pages/About.jsx
+++ Alhekma/src/pages/About.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import img from '../assets/images/s2.jpg'
 
 const About = () => {
   return (
     <div>
-      <h1>ABout</h1>
+
+      <h1 className='text-4xl mb-4 text-center mt-10' >History</h1>
+      <div className='container d-flex justify-center gap-12  p-5' style={{boxShadow:"0 0 3px"}}>
+        <div className='content '>
+          Officia nisi ut officia ipsum consectetur nulla commodo cillum nulla est ea fugiat. Lorem consequat exercitation sit amet ea aliqua ea et nostrud ea cillum nisi dolor. Mollit laborum aliquip esse sunt mollit dolor magna dolore culpa. Ex qui mollit exercitation deserunt culpa duis Lorem quis mollit adipisicing nostrud amet. Voluptate cupidatat anim adipisicing nulla adipisicing laborum minim incididunt dolore fugiat Lorem id. Ea sit occaecat consequat officia amet do mollit ad consequat eu velit qui anim cupidatat. Enim veniam do exercitation velit pariatur veniam laborum nulla voluptate.
+        </div>
+
+
+        <div className='img'>
+          <img src={img} alt='media'/>
+        </div>
+      
+      </div>
+
+{/* --------- */}
+
+      <div className='container mt-8'>
+         <h1 className='text-4xl'>Scope</h1>
+          Ipsum reprehenderit voluptate non consequat sit velit nisi deserunt Lorem pariatur amet sunt. Ex aliquip eiusmod consectetur veniam amet non ad incididunt sint adipisicing Lorem et. Nisi duis ut ex occaecat. Labore nulla pariatur consequat anim fugiat. Consectetur ut esse cillum nostrud id culpa nostrud. Consequat consequat eu exercitation qui. Sint consequat laborum id sit ad pariatur magna excepteur velit in commodo aute.
+      </div>
+
+
+      <div className='container mt-8'>
+         <h1 className='text-4xl'>Out Team</h1>
+          Ipsum reprehenderit voluptate non consequat sit velit nisi deserunt Lorem pariatur amet sunt. Ex aliquip eiusmod consectetur veniam amet non ad incididunt sint adipisicing Lorem et. Nisi duis ut ex occaecat. Labore nulla pariatur consequat anim fugiat. Consectetur ut esse cillum nostrud id culpa nostrud. Consequat consequat eu exercitation qui. Sint consequat laborum id sit ad pariatur magna excepteur velit in commodo aute.
+      </div>
+
+
+
+      <div className='container mt-8'>
+         <h1 className='text-4xl'>Accreditations</h1>
+          Ipsum reprehenderit voluptate non consequat sit velit nisi deserunt Lorem pariatur amet sunt. Ex aliquip eiusmod consectetur veniam amet non ad incididunt sint adipisicing Lorem et. Nisi duis ut ex occaecat. Labore nulla pariatur consequat anim fugiat. Consectetur ut esse cillum nostrud id culpa nostrud. Consequat consequat eu exercitation qui. Sint consequat laborum id sit ad pariatur magna excepteur velit in commodo aute.
+      </div>
+
+
+      <div className='container mt-8'>
+         <h1 className='text-4xl mb-4'>Contact Us</h1>
+         <form>
+          <input type='email' className='form-control mb-4' placeholder='Enter your email address'/>
+          <input type='text' className='form-control mb-4 ' placeholder='Enter your name'/>
+          <button className='btn btn-success py-2 px-4 '>Send</button>
+         </form>
+      </div>
+
     </div>
   )
 }
--- Alhekma/src/pages/PCare.jsx
+++ Alhekma/src/pages/PCare.jsx
@@ -3,7 +3,83 @@ import React from 'react'
 const PCare = () => {
   return (
     <div>
-      <h1>Patient Cere</h1>
+      <h1 className='text-4xl'>Patient Cere</h1>
+
+
+      <h1 className='text-4xl text-gray-600'>Admission Guide</h1>
+
+      <div className='container mt-8'>
+         <h1 className='text-4xl'>Overview</h1>
+         Whether you are booking an appointment with us for the first time, looking for help and advice
+        on paying for private treatment, or require details about our admissions and support services,
+        this section provides you with information on everything from your initial consultation to
+        post-treatment care.
+      </div>
+
+
+      <div className='container mt-8'>
+         <h1 className='text-4xl'>Important Information for Patients</h1>
+         In line with our commitment to high quality customer care, Al Hekma Hospital aims to provide
+        clear and transparent information to patients about all aspects of their treatment. For this
+        reason, it is our policy to quote and charge separately for individual tests and procedures
+        carried out in the Outpatients Department.
+        Please be aware therefore that the cost of any tests or procedures, such as blood tests, scans
+        and x-rays that are carried out at Al Hekma at the request of your consultant, are additional to
+        the consultation fee and will be shown as separate charges in your bill. If you are in any doubt
+        as to whether a test or procedure will attract a new charge, please speak to the nurse in
+        advance.
+        If your treatment is covered by insurance, you are advised to check with your insurance
+        company before any tests or procedures are carried out.
+        If you do have any questions however please speak to your nurse before procedures are carried
+        out.   
+      </div>
+
+
+      <div className='container mt-8'>
+          <h1 className='text-4xl mt-8'>Rights & Responsibilities</h1>
+          Ipsum reprehenderit voluptate non consequat sit velit nisi deserunt Lorem pariatur amet sunt. Ex aliquip eiusmod consectetur veniam amet non ad incididunt sint adipisicing Lorem et. Nisi duis ut ex occaecat. Labore nulla pariatur consequat anim fugiat. Consectetur ut esse cillum nostrud id culpa nostrud. Consequat consequat eu exercitation qui. Sint consequat laborum id sit ad pariatur magna excepteur velit in commodo aute.
+      </div>
+
+
+      <div className='container mt-8'>
+          <h1 className='text-4xl mt-8 text-gray-600'>Hospitality</h1>
+       </div>
+    
+
+      <div className='container mt-8'>
+          <h1 className='text-4xl mt-8 text-gray-600'>Patient Safety</h1>
+     </div>
+
+
+      <div className='container mt-8'>
+          <h1 className='text-4xl mt-8'>Infection Contol</h1>
+              Al Hekma Hospital is equipped with a highly-skilled Infection control team that constantly
+              ensures a safe and healthy working environment for our employees as well as a safe and
+              infection-free hospital to our patients and visitors.
+
+<br/>
+<br/>
+<br/>
+<br/>
+The team's role is to promote, train and educate our staff on ways to prevent the spread of
+infections, as well as determining the following:
+<ul>
+  <li>The implementation of infection control parameters all around the hospital.</li>
+  <li>The compliance of the staff members to the infection control standard precautions and
+proper hand hygiene.</li>
+  <li>The precise sterilisation processes</li>
+  <li>The proper environmental cleaning and disinfection process inside the hospital.</li>
+  <li>The proper implementation of the transmission-based precautions for isolated patients.</li>
+</ul>
+
+      
+</div>
+
+
+<h1 className='text-4xl mt-8 text-gray-600'>Partnerships</h1>
+
+<h1 className='text-4xl mt-8 text-gray-600'>Insurance</h1>
+
     </div>
   )
 }
